8f51a1a086d9b9e5b96a160cb74bcc29a7ea13e6
Mohammed Farag <mohammed.farag@Egypt.TE-Data.core>
2017-02-14 10:55:59 +0200

    Setup start script and heroku port.

--- server.js
+++ server.js
@@ -1,27 +1,39 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs=require('fs');
 
+// configuration to use enviroment variable port for heroku.com and set default 3000
+const port=process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname+'/views/partials');
 app.set('view engine', 'hbs');
-//app.use(express.static(__dirname + '/public'));
 
+app.use((req,res,next)=>{
+  var now=new Date().toString();
+  var log=`${now} : ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log',log+'\n',()=>{});
+  next();
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear',()=>{return new Date().getFullYear();});
 hbs.registerHelper('screamIt',(text)=> {
   return text.toUpperCase();
 });
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to my website',
-    currentYear: new Date().getFullYear()
+    welcomeMessage: 'Welcome to my website'
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
+    pageTitle: 'About Page'
   });
 });
 
@@ -32,6 +44,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
